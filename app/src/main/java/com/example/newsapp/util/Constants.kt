08fe1de619bcfb83f9eb134110e7e6235f7af1e5
MaxA07 <amorfis77@gmail.com>
2023-04-12 01:08:05 +0300

    added retrofit and room db

---
+++ app/src/main/java/com/example/newsapp/util/Constants.kt
@@ -0,0 +1,9 @@
+package com.example.newsapp.util
+
+class Constants {
+
+    companion object {
+        const val API_KEY = "af9845be6f824592aa0edee2e3524e9f"
+        const val BASE_URL = "https://newsapi.org"
+    }
+}
