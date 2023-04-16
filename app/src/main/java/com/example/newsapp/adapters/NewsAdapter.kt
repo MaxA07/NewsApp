fb4d8928bca8398f2cd563aa08fc98d7b55b4958
MaxA07 <amorfis77@gmail.com>
2023-04-16 23:12:01 +0300

    added viewmodel to breaking news, breaking news factory, got and showed data from api

---
+++ app/src/main/java/com/example/newsapp/adapters/NewsAdapter.kt
@@ -0,0 +1,64 @@
+package com.example.newsapp.adapters
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.AsyncListDiffer
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.example.newsapp.R
+import com.example.newsapp.models.Article
+
+class NewsAdapter : RecyclerView.Adapter<NewsAdapter.AreicleViewHolder>() {
+
+
+    inner class AreicleViewHolder(itemView: View): RecyclerView.ViewHolder(itemView)
+
+    private val differCallback = object : DiffUtil.ItemCallback<Article>() {
+
+        override fun areItemsTheSame(oldItem: Article, newItem: Article): Boolean {
+            return oldItem.url == newItem.url
+        }
+
+        override fun areContentsTheSame(oldItem: Article, newItem: Article): Boolean {
+            return oldItem == newItem
+        }
+    }
+
+    val differ = AsyncListDiffer(this, differCallback)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): AreicleViewHolder {
+        return AreicleViewHolder(
+            LayoutInflater.from(parent.context).inflate(
+                R.layout.item_article_preview,
+                parent,
+                false
+            )
+        )
+    }
+
+    override fun onBindViewHolder(holder: AreicleViewHolder, position: Int) {
+        val article = differ.currentList[position]
+        holder.itemView.apply {
+            Glide.with(this).load(article.urlToImage).into(findViewById(R.id.ivArticleImage))
+            findViewById<TextView>(R.id.tvSource).text = article.source.name
+            findViewById<TextView>(R.id.tvTitle).text = article.title
+            findViewById<TextView>(R.id.tvDescription).text = article.publishedAt
+            setOnClickListener {
+                onItemClickListener?.let { it(article) }
+            }
+        }
+    }
+
+    override fun getItemCount(): Int {
+        return differ.currentList.size
+    }
+
+    private var onItemClickListener: ((Article) -> Unit)? = null
+
+    fun setOnItemClickListener(listener: ((Article) -> Unit)) {
+        onItemClickListener = listener
+    }
+}
